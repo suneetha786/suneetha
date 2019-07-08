65fe65730de8be2553f86af16a06a77a565e8c5c
suneetha <suneetha@LAPTOP-A0RHBCJJ>
2019-07-08 17:48:53 +0530

    prac com1 from eclipse

--- sbIntegrationDemo/src/main/java/com/sbmvc/SbIntegrationDemoApplication.java
+++ sbIntegrationDemo/src/main/java/com/sbmvc/SbIntegrationDemoApplication.java
@@ -9,6 +9,6 @@ public class SbIntegrationDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SbIntegrationDemoApplication.class, args);
 	}
-// prac
+// prac 1234
 }
 
